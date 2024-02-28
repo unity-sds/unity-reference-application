280f2a6cc89b006b70dfd1e35138419294395573
Tom Barber <tom@spicule.co.uk>
2024-02-28 18:34:31 +0000

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -5,7 +5,7 @@ resource "aws_efs_file_system" "demo_config_efs" {
   }
 }
 resource "aws_security_group" "efs_sg" {
-  name        = "${var.deployment_name}-efs-security-group"
+  name        = "${var.deployment_name}-reference-app-efs-security-group"
   description = "Security group for EFS"
   vpc_id      = data.aws_ssm_parameter.vpc_id.value
 
