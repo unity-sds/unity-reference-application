c1563858fc17e0b72f8e65353e0c42372a6ebf33
Tom Barber <tom@spicule.co.uk>
2024-02-28 18:24:29 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,17 @@ variable "tags" {
   description = "AWS Tags"
   type = map(string)
 }
+variable "project"{
+  description = "The unity project its installed into"
+  type = string
+  default = "UnknownProject"
+}
 
+variable "venue" {
+  description = "The unity venue its installed into"
+  type = string
+  default = "UnknownVenue"
+}
 variable "deployment_name" {
   description = "The deployment name"
   type        = string
