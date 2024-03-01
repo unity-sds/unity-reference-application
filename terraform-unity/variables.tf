6c6a33ab56a9fb032738d79016b29415ba39c73b
barber <tom@spicule.co.uk>
2024-03-01 21:19:02 +0000

    update

--- terraform-unity/variables.tf
+++ terraform-unity/variables.tf
@@ -2,18 +2,30 @@ variable "tags" {
   description = "AWS Tags"
   type = map(string)
 }
+variable "project"{
+  description = "The unity project its installed into"
+  type = string
+  default = "UnknownProject"
+}
 
+variable "venue" {
+  description = "The unity venue its installed into"
+  type = string
+  default = "UnknownVenue"
+}
 variable "deployment_name" {
   description = "The deployment name"
   type        = string
 }
-
+variable "installprefix" {
+  description = "The management console install prefix"
+  type = string
+  default = "UnknownPrefix"
+}
 variable "template" {
   default = <<EOT
-<VirtualHost *:8080>
                   RewriteEngine on
                   ProxyPass /sample http://test-demo-alb-616613476.us-west-2.elb.amazonaws.com:8888/sample/hello.jsp
                   ProxyPassReverse /sample http://test-demo-alb-616613476.us-west-2.elb.amazonaws.com:8888/sample/hello.jsp
-</VirtualHost>
 EOT
 }
